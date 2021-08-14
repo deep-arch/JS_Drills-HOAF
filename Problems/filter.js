16da0aec91f34aa326328a60fe36ad705ce14f7d
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-14 15:46:05 +0530

    Filter and testFilter

---
+++ Problems/filter.js
@@ -0,0 +1,17 @@
+//function filter(elements, cb) {
+  // Do NOT use .filter, to complete this function.
+  // Similar to `find` but you will return an array of all elements that passed the truth test
+  // Return an empty array if no elements pass the truth test
+//}
+
+
+export function filter(elements, cb) 
+{
+  let result = [];
+  for (let i = 0; i < elements.length; i++) 
+  {
+    if (cb(elements[i], i)) 
+        result.push(cb(elements[i], i));
+  }
+    return result;
+}
---
+++ Problems/test/testFilter.js
@@ -0,0 +1,10 @@
+import { items } from "../../Inventory/inventory.js";
+import { filter } from "../filter.js";
+const cb = (value, index) => 
+{
+  //if(enter your condition)
+  //{
+      //Add your code
+  //}
+};
+console.log(filter(items,cb));
