4f6bd4abd8df950565d206b3652b8bdd4d0b60be
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-14 15:41:42 +0530

    Each and testEach

---
+++ Problems/each.js
@@ -0,0 +1,14 @@
+//function each(elements, cb) {
+  // Do NOT use forEach to complete this function.
+  // Iterates over a list of elements, yielding each in turn to the `cb` function.
+  // This only needs to work with arrays.
+  // You should also pass the index into `cb` as the second argument
+  // based off http://underscorejs.org/#each
+// }
+
+
+export function each(elements, cb) 
+{
+  for (let i = 0; i < elements.length; i++) 
+    cb(elements[i], i);
+}
---
+++ Problems/test/testEach.js
@@ -0,0 +1,8 @@
+import { items } from "../../Inventory/inventory.js";
+import { each } from "../each.js";
+const cb = (value, index) => 
+{ 
+  //console.log(value) *to test*
+  //Add your code  
+};
+each(items,cb);
