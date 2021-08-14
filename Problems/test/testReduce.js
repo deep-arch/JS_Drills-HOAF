65bd3ad4d487fcf596deb112bfbf4d0777692bd6
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-14 15:50:39 +0530

    Reduce and testReduce

---
+++ Problems/test/testReduce.js
@@ -0,0 +1,11 @@
+import { items } from "../../Inventory/inventory.js";
+import { reduce } from "../reduce.js";
+const cb = (startingValue, currentValue) => 
+{
+  // startingValue + currentValue; *to test*
+  //if(enter your condition)
+  //{
+      //Add your code
+  //}
+};
+console.log(reduce(items,cb));
